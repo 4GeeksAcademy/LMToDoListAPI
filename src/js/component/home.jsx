5d44abdb73c9b011089653359e47736ad040a0ed
Letimachado <leti.machado.gerez@gmail.com>
2024-07-16 23:20:46 +0000

    Fiiiin

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,30 +1,100 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
-const Home = () => {
+const Home = () => {	
+const apiUrl = 'https://playground.4geeks.com/todo'
 
-//let nuevoTodo = "";
-const [nuevoTodo, setNuevoTodo] = useState("")
+const [nuevoTodo, setNuevoTodo] = useState("")	//estado de los items
 
-const [todos, setTodos] = useState([]);
+const [todos, setTodos] = useState([]);  //estado de la lista entera
 
-
-const onkeydown = (event) => {
-	if (event.key === "Enter") {
-		setTodos([...todos, nuevoTodo])
-	setNuevoTodo("")}
-	
+const onkeydown = (event) => { // agregar a la lista al dar enter
+	if (event.key === "Enter") {   //enter debe empezar con mayuscula
+		agregarTodo()
+	setNuevoTodo("")}	
 };
 
-const deleteTodo = (indice) => {
-	const listaNueva = todos.filter((todo, i) => i !== indice)
-	setTodos(listaNueva);
-}
+
 
 const handleChange = (event) => {
 	setNuevoTodo(event.target.value);
 }
 
 
+function crearUsuario() {   //Hago el fetch para obtener los datos de la API
+	fetch(`${apiUrl}/users/letimachado`, {method: "POST"})
+	.then((response)=> response.json())
+	.then((data)=> console.log(data))
+	.catch((error)=> console.log(error))
+}
+
+function obtenerInfo() {   
+	fetch(`${apiUrl}/users/letimachado`)
+	.then((response)=> {
+		console.log(response);
+		if(response.status === 404) {
+			crearUsuario()
+			return false
+		}
+		return response.json()
+	})
+	.then((data)=> {
+		if(data) {
+			setTodos(data.todos)
+		}
+	})
+	.catch((error)=> console.log(error))
+}
+
+function agregarTodo() {   
+	fetch(`${apiUrl}/todos/letimachado`, {
+		method: "POST",
+		body: JSON.stringify({
+			"label": nuevoTodo,
+			"is_done": false
+		  }),
+		headers: {
+			"Content-Type": "application/json"
+		}
+
+	})
+	.then((response)=> {
+		console.log(response);
+		if(response.ok) {
+			return response.json()
+		}
+		return false
+	})
+	.then((data)=> {
+		if(data) {
+			setTodos(todos.concat(data))
+		}
+	})
+	.catch((error)=> console.log(error))
+}
+
+
+
+
+
+function eliminarTodo(id) {   
+	fetch(`${apiUrl}/todos/${id}`, {method: "DELETE"})
+	.then((response)=> {
+		console.log(response);
+		if(response.ok) {
+			const listaNueva = todos.filter((todo) => todo.id !== id)
+			setTodos(listaNueva);
+		}
+		return false
+	})
+	.catch((error)=> console.log(error))
+}
+
+
+
+useEffect(()=> {
+	obtenerInfo()
+	
+},[]);
 
   
 
@@ -38,10 +108,10 @@ const handleChange = (event) => {
      				 <input type="text" className="form-control" onChange={handleChange} value={nuevoTodo} onKeyDown = {(e) => onkeydown(e)} />
     			</div>
     			<ul className="list-group list-group-flush">
-       			 {todos.map((todo, indice) => {
+       			 {todos.map((todo) => {
             		return(
-               		 <li className="list-group-item">
-                    {todo} <button onClick={() => deleteTodo(indice)} type="button" className="btn-close " aria-label="Close"></button>
+               		 <li className="list-group-item" key={todo.id}>
+                    {todo.label} <button onClick={() => eliminarTodo(todo.id)} type="button" className="btn-close " aria-label="Close"></button>
                 	</li>
             		)
        			 })}
